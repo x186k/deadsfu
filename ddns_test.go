d118934f7e5514a618df0017b6f34e832edb5056
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2021-03-02 22:04:49 -0800

    New DDNS tests

---
+++ ddns_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	//"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDDNS5TokenMaker(t *testing.T) {
+
+	k := ddns5Token()
+	require.Equal(t, 32, len(k))
+
+	kk := ddns5Token()
+	require.Equal(t,k,kk)
+
+	_=os.Remove( "/tmp/ddns5.txt")
+
+	kkk:= ddns5Token()
+	require.Equal(t, 32, len(kkk))
+
+	require.NotEqual(t,k,kkk)
+
+}
