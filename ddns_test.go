ba4fc25370de0663ce3e882a9ef5bf76c276c020
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-04-02 15:03:51 -0700

    Add DDNS5 support and revamp testing

--- ddns_test.go
+++ ddns_test.go
@@ -12,99 +12,141 @@ import (
 	"testing"
 	"time"
 
+	//"github.com/felixge/fgprof"
+
+	//"github.com/felixge/fgprof"
 	"github.com/libdns/cloudflare"
+	"github.com/libdns/duckdns"
 	"github.com/miekg/dns"
-
+	"github.com/x186k/ddns5libdns"
 	//"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/require"
 )
-
-func TestDDNS5TokenMaker(t *testing.T) {
-
-	k := ddns5com_Token()
-	require.Equal(t, 32, len(k))
-
-	kk := ddns5com_Token()
-	require.Equal(t, k, kk)
-
-	_ = os.Remove("/tmp/ddns5.txt")
-
-	kkk := ddns5com_Token()
-	require.Equal(t, 32, len(kkk))
-
-	require.NotEqual(t, k, kkk)
-
-}
-
-var cloudflareToken = os.Getenv("CLOUDFLARE_TOKEN")
-var libdnsProvider = &cloudflare.Provider{APIToken: cloudflareToken}
 
 var ctx = context.Background()
 
-// Create and seed the generator.
-// Typically a non-fixed seed should be used, such as time.Now().UnixNano().
-// Using a fixed seed will produce the same output on every run.
-
-func init() {
-	if cloudflareToken == "" {
-		panic("no token")
-	}
-
-	//Debug = true
-}
-
-func TestARecord(t *testing.T) {
+//random normalized ipv4
+func randIPv4() string {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	ipRaw := make([]byte, 4)
 	binary.LittleEndian.PutUint32(ipRaw, r.Uint32())
 	randipaddr := net.IP(ipRaw).String()
-	fmt.Println("Random IPv4 unnorm: ", randipaddr)
-	fmt.Println("Random IPv4 norm: ", NormalizeIP(randipaddr, dns.TypeA))
-
-	fqdn := "_testipv4.sfu1.com"
-
-	err := ddnsSetRecord(ctx, libdnsProvider, fqdn, 2, randipaddr, dns.TypeA)
-	checkPanic(err)
-
-	err = ddnsWaitUntilSet(ctx, libdnsProvider, fqdn, randipaddr, dns.TypeA)
-	if err != nil {
-		t.Error("Wait failed", err)
-	}
+	return NormalizeIP(randipaddr, dns.TypeA)
 }
-func TestAAAARecord(t *testing.T) {
+
+//random normalized ipv6
+func randIPv6() string {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	ipRaw := make([]byte, 16)
 	_, err := r.Read(ipRaw)
 	checkPanic(err)
 	ipRaw[0] = 0x00 // testing zero fill
 	randipaddr := net.IP(ipRaw).String()
-	fmt.Println("Random IPv6 unnorm: ", randipaddr)
-	fmt.Println("Random IPv6 norm: ", NormalizeIP(randipaddr, dns.TypeAAAA))
 
-	var dnsname = "_testipv6.sfu1.com"
-
-	err = ddnsSetRecord(ctx, libdnsProvider, dnsname, 2, randipaddr, dns.TypeAAAA)
-	checkPanic(err)
-
-	err = ddnsWaitUntilSet(ctx, libdnsProvider, dnsname, randipaddr, dns.TypeAAAA)
-	if err != nil {
-		t.Error("Wait failed", err)
-	}
+	return NormalizeIP(randipaddr, dns.TypeAAAA)
 }
 
-func TestTXTRecord(t *testing.T) {
-	var fqdn = "_testtxt.sfu1.com"
-	var val = "xyzzy"
+func TestDDNSCloudflare(t *testing.T) {
+	ddnsutilDebug = true
 
-	checkPanic(ddnsRemoveAddrs(ctx, libdnsProvider, fqdn, 2, dns.TypeTXT))
+	token := os.Getenv("CLOUDFLARE_TOKEN")
+	if token == "" {
+		println("CLOUDFLARE_TOKEN", "is unset, will skip testing cloudflare")
+		return
+	}
+	fqdn := os.Getenv("CLOUDFLARE_TEST_FQDN")
+	if fqdn == "" {
+		println("CLOUDFLARE_TEST_FQDN", "is unset, will skip testing cloudflare")
+		return
+	}
 
-	err := ddnsSetRecord(ctx, libdnsProvider, fqdn, 2, val, dns.TypeTXT)
-	checkPanic(err)
+	provider := &cloudflare.Provider{APIToken: token}
 
-	err = ddnsWaitUntilSet(ctx, libdnsProvider, fqdn, val, dns.TypeTXT)
+	testProvider(t, provider, fqdn)
+
+}
+
+func TestDDNS5API(t *testing.T) {
+	ddnsutilDebug = true
+
+	//token := strings.Repeat("a", 32)
+	fqdn := "test99.ddns5.com"
+
+	provider := &ddns5libdns.Provider{APIToken: ""}
+
+	//ddnsutilDebug = true
+	testProvider(t, provider, fqdn)
+
+}
+
+func TestDDNSDuckdnsAPI(t *testing.T) {
+
+	//ctx = httpTrace(ctx)
+
+	ddnsutilDebug = true
+
+	token := os.Getenv("DUCKDNS_TOKEN")
+	if token == "" {
+		println("DUCKDNS_TOKEN", "is unset, will skip testing duckdns")
+		return
+	}
+	fqdn := os.Getenv("DUCKDNS_TEST_FQDN")
+	if fqdn == "" {
+		println("DUCKDNS_TEST_FQDN", "is unset, will skip testing duckdns")
+		return
+	}
+
+	provider := &duckdns.Provider{APIToken: token}
+
+	testProvider(t, provider, fqdn)
+}
+
+func testProvider(t *testing.T, provider DDNSProvider, fqdn string) {
+	_ = httpTrace // silence warnings about unused
+	// turn on tracing
+	//ctx = httpTrace(ctx)
+	_, zone := splitFQDN(fqdn, 2)
+
+	testRemoveSetWaitFindRemoveFind(t, provider, zone, fqdn, randIPv4(), dns.TypeA)
+	testRemoveSetWaitFindRemoveFind(t, provider, zone, fqdn, randIPv6(), dns.TypeAAAA)
+	testRemoveSetWaitFindRemoveFind(t, provider, zone, fqdn, randomHex(10), dns.TypeTXT)
+}
+
+func testRemoveSetWaitFindRemoveFind(t *testing.T, provider DDNSProvider, zone string, fqdn string, val string, dnstype uint16) {
+	var err error
+
+	err = ddnsRemoveAddrs(ctx, provider, fqdn, 2, dnstype)
+	if err != nil {
+		t.Error("ddnsRemoveAddrs failed", err)
+	}
+
+	err = ddnsSetRecord(ctx, provider, fqdn, 2, val, dnstype)
+	if err != nil {
+		t.Error("ddnsRemoveAddrs failed", err)
+	}
+
+	err = ddnsWaitUntilSet(ctx, fqdn, val, dnstype)
 	if err != nil {
 		t.Error("Wait failed", err)
 	}
+
+	// found, err := ddnsFindAddrs(zone, fqdn, dnstype)
+	// if err != nil {
+	// 	t.Error("ddnsFindAddrs failed", err)
+	// }
+
+	// require.Equal(t, val, found)
+
+	err = ddnsRemoveAddrs(ctx, provider, fqdn, 2, dnstype)
+	if err != nil {
+		t.Error("ddnsRemoveAddrs failed", err)
+	}
+
+	// found, err = ddnsFindAddrs(zone, fqdn, dnstype)
+	// if err != nil {
+	// 	t.Error("ddnsFindAddrs failed2", err)
+	// }
+
+	// require.Equal(t, "", found)
 }
 
 func httpTrace(ctx context.Context) context.Context {
@@ -113,7 +155,7 @@ func httpTrace(ctx context.Context) context.Context {
 		GetConn: func(hostPort string) {
 		},
 		GotConn: func(connInfo httptrace.GotConnInfo) {
-			fmt.Printf("Got Conn: %+v\n", connInfo)
+			//fmt.Printf("Got Conn: %+v\n", connInfo)
 		},
 		PutIdleConn: func(err error) {
 		},
@@ -127,20 +169,26 @@ func httpTrace(ctx context.Context) context.Context {
 		DNSStart: func(httptrace.DNSStartInfo) {
 		},
 		DNSDone: func(dnsInfo httptrace.DNSDoneInfo) {
-			fmt.Printf("DNS Info: %+v\n", dnsInfo)
+			//fmt.Printf("DNS Info: %+v\n", dnsInfo)
 		},
 		ConnectStart: func(network string, addr string) {
-			fmt.Println("ConnectStart:", addr)
+			//fmt.Println("ConnectStart:", addr)
 		},
 		ConnectDone: func(network string, addr string, err error) {
-			fmt.Println("ConnectDone:", addr)
+			//fmt.Println("ConnectDone:", addr)
 		},
 		TLSHandshakeStart: func() {
 		},
 		// TLSHandshakeDone: func(tls.ConnectionState, error) {
 		// },
 		WroteHeaderField: func(key string, value []string) {
-			fmt.Printf("WroteHeaderField: %s %+v\n", key, value)
+			switch key {
+			case ":path":
+				fallthrough
+			case ":method":
+				fmt.Printf("trace %s %+v\n", key, value)
+			}
+
 		},
 		WroteHeaders: func() {
 		},
@@ -153,15 +201,6 @@ func httpTrace(ctx context.Context) context.Context {
 
 	return httptrace.WithClientTrace(ctx, trace)
 	//req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
-}
-
-func TestDel(t *testing.T) {
-	var fqdn = "_testtxt.sfu1.com"
-
-	ctx = httpTrace(ctx)
-
-	checkPanic(ddnsRemoveAddrs(ctx, libdnsProvider, fqdn, 2, dns.TypeTXT))
-
 }
 
 // curl -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
