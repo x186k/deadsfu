29a64389a869254769a961345fa2e517e5a544fb
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-07-11 15:09:29 -0700

    Switch to go 1.13 errors.* proxy decoder

--- proxy.go
+++ proxy.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"strings"
 	"time"
 
 	"golang.org/x/net/proxy"
@@ -54,49 +53,40 @@ func canConnectThroughProxy(proxyaddr string, tcpaddr *net.TCPAddr, network stri
 	// we ask the proxy for the given network
 	conn, err := contextDialer.DialContext(ctx, network, tcpaddr.String())
 	if err != nil {
-
-		println(888, err.Error())
-		println(999, errors.Is(err, os.ErrDeadlineExceeded))
-		println(111)
 		for xx := err; xx != nil; xx = errors.Unwrap(xx) {
-			fmt.Printf("%#v\n", xx)
-
+			//fmt.Printf("%#v\n", xx)
 			var operr *net.OpError
 			if errors.As(xx, &operr) {
-				println(222, operr.Op == "read")
-				println(333, errors.Is(xx, os.ErrDeadlineExceeded))
-				println(444, operr.Err == os.ErrDeadlineExceeded)
+				readop := operr.Op == "read"
+				dialop := operr.Op == "dial"
+				iotimeout := operr.Err == os.ErrDeadlineExceeded //also errors.Is(xx, os.ErrDeadlineExceeded)
 
-				fmt.Println("Failed at op:", operr.Op)
+				if readop && iotimeout {
+					proxyOK = true
+					portOpen = false
+					return
+				} else if dialop && iotimeout {
+					proxyOK = false
+					portOpen = false
+					return
+				}
 			} else {
 				fmt.Println(err)
 			}
 		}
-		println(111)
+		// unexpected issue with proxy, but we stay silent
+		// unless debugging is on
+		// maybe Cam didn't pay proxy bill
+		log.Println("unexpected proxy behavior")
 		for xx := err; xx != nil; xx = errors.Unwrap(xx) {
-			fmt.Printf("%+v\n", xx)
+			log.Printf("%#v\n", xx)
 		}
-		println(111)
-
-		readtcp := strings.Contains(err.Error(), " read tcp ")
-		dialtcp := strings.Contains(err.Error(), " dial tcp ")
-		iotimeout := strings.Contains(err.Error(), "i/o timeout")
-
-		if iotimeout && readtcp {
-			proxyOK = true
-			portOpen = false
-		} else if iotimeout && dialtcp {
-			proxyOK = false
-			portOpen = false
-		}
-		// sometime else is going on, but we are going to stay silent
-		// the user isn't going to know what to do
 
 		return
 	}
 	conn.Close()
 
-	// if we got here, I got through the proxy and connected to myself
+	// if we got here, I got through the proxy and connected to myself (I think)
 
 	proxyOK = true
 	portOpen = true
