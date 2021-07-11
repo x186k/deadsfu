c8b58c66b80581224954a7e1a86b5beca28ad632
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-07-11 14:51:31 -0700

    debugging the proxy lots of printing and dumping

--- proxy.go
+++ proxy.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"strings"
 	"time"
 
@@ -52,7 +54,30 @@ func canConnectThroughProxy(proxyaddr string, tcpaddr *net.TCPAddr, network stri
 	// we ask the proxy for the given network
 	conn, err := contextDialer.DialContext(ctx, network, tcpaddr.String())
 	if err != nil {
+
 		println(888, err.Error())
+		println(999, errors.Is(err, os.ErrDeadlineExceeded))
+		println(111)
+		for xx := err; xx != nil; xx = errors.Unwrap(xx) {
+			fmt.Printf("%#v\n", xx)
+
+			var operr *net.OpError
+			if errors.As(xx, &operr) {
+				println(222, operr.Op == "read")
+				println(333, errors.Is(xx, os.ErrDeadlineExceeded))
+				println(444, operr.Err == os.ErrDeadlineExceeded)
+
+				fmt.Println("Failed at op:", operr.Op)
+			} else {
+				fmt.Println(err)
+			}
+		}
+		println(111)
+		for xx := err; xx != nil; xx = errors.Unwrap(xx) {
+			fmt.Printf("%+v\n", xx)
+		}
+		println(111)
+
 		readtcp := strings.Contains(err.Error(), " read tcp ")
 		dialtcp := strings.Contains(err.Error(), " dial tcp ")
 		iotimeout := strings.Contains(err.Error(), "i/o timeout")
