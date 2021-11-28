86a30dd8156fe56223b71e9de52a5269f51caa90
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-11-28 15:25:28 -0800

    add dotFileHidingFileSystem support
    and redirecting /foo to /index.html

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"context"
 	"embed"
+	"errors"
 	"math/rand"
 	"net"
 	"path"
@@ -391,7 +392,9 @@ func setupMux(conf SfuConfig) (*http.ServeMux, error) {
 		f, err := fs.Sub(htmlContent, "html")
 		checkFatal(err)
 
-		rootmux = handleSDPWarning(http.FileServer(http.FS(f)))
+		fsys := dotFileHidingFileSystemPlus{http.FS(f)}
+
+		rootmux = handleSDPWarning(http.FileServer(fsys))
 
 	} else if httpPrefix || httpsPrefix {
 
@@ -412,7 +415,10 @@ func setupMux(conf SfuConfig) (*http.ServeMux, error) {
 		}
 
 		f := os.DirFS(*htmlSource)
-		rootmux = handleSDPWarning(http.FileServer(http.FS(f)))
+
+		fsys := dotFileHidingFileSystemPlus{http.FS(f)}
+
+		rootmux = handleSDPWarning(http.FileServer(fsys))
 
 	}
 
@@ -570,6 +576,9 @@ func pubHandler(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	roomname := r.URL.Query().Get("room") // "" is permitted, most common room name!
+	// if roomname == "" {
+	// 	// no error!
+	// }
 	link := getPubSubLink(roomname)
 
 	if !link.pubSema.TryAcquire(1) {
@@ -1682,4 +1691,61 @@ func isMysteryOBSFTL(p rtp.Packet) (ok bool) {
 		}
 	}
 	return true
+}
+
+// containsDotFile reports whether name contains a path element starting with a period.
+// The name is assumed to be a delimited by forward slashes, as guaranteed
+// by the http.FileSystem interface.
+func containsDotFile(name string) bool {
+	parts := strings.Split(name, "/")
+	for _, part := range parts {
+		if strings.HasPrefix(part, ".") {
+			return true
+		}
+	}
+	return false
+}
+
+// dotFileHidingFile is the http.File use in dotFileHidingFileSystem.
+// It is used to wrap the Readdir method of http.File so that we can
+// remove files and directories that start with a period from its output.
+type dotFileHidingFile struct {
+	http.File
+}
+
+// Readdir is a wrapper around the Readdir method of the embedded File
+// that filters out all files that start with a period in their name.
+func (f dotFileHidingFile) Readdir(n int) (fis []fs.FileInfo, err error) {
+	files, err := f.File.Readdir(n)
+	for _, file := range files { // Filters out the dot files
+		if !strings.HasPrefix(file.Name(), ".") {
+			fis = append(fis, file)
+		}
+	}
+	return
+}
+
+// dotFileHidingFileSystemPlus is an http.FileSystem that hides
+// hidden "dot files" from being served.
+type dotFileHidingFileSystemPlus struct {
+	http.FileSystem
+}
+
+// Open is a wrapper around the Open method of the embedded FileSystem
+// that serves a 403 permission error when name has a file or directory
+// with whose name starts with a period in its path.
+func (fsys dotFileHidingFileSystemPlus) Open(name string) (http.File, error) {
+	if containsDotFile(name) { // If dot file, return 403 response
+		return nil, fs.ErrPermission
+	}
+
+	file, err := fsys.FileSystem.Open(name)
+	// next three lines turn /foo/bar into /index.html
+	if errors.Is(err, fs.ErrNotExist) {
+		file, err = fsys.FileSystem.Open("/index.html")
+	}
+	if err != nil {
+		return nil, err
+	}
+	return dotFileHidingFile{file}, err
 }
