b31ad58dfb0792c7769a7a287a657fbeeb1839b8
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-12-09 17:01:32 -0800

    shorten --dial-upstream-url

--- flags.go
+++ flags.go
@@ -22,7 +22,7 @@ var httpsDnsRegisterIp = pflag.BoolP("https-dns-register-ip", "s", false, "DNS-R
 var httpsDnsRegisterIpPublic = pflag.BoolP("https-dns-register-ip-public", "t", false, "DNS-Register the IP of this box, at provider, for name: --https-domain. Detects public addrs")
 var httpsUseDns01Challenge = pflag.BoolP("https-dns01-challenge", "u", false, "When registering at Let's Encrypt, use the DNS challenge, not HTTP/HTTPS. Recommended behind firewalls")
 
-var dialUpstreamUrlFlag = pflag.StringP("dial-upstream-url", "d", "", "Specify a URL for upstream SFU. No path, no params. Used for SFU chaining!. Upstream dial triggered on subscriber connection to room.")
+var dialUpstreamUrlFlag = pflag.StringP("dial-upstream", "d", "", "Specify a URL for upstream SFU. No path, no params. Used for SFU chaining!. Upstream dial triggered on subscriber connection to room.")
 
 var iceCandidateHost = pflag.String("ice-candidate-host", "", "For forcing the ice host candidate IP address")
 var iceCandidateSrflx = pflag.String("ice-candidate-srflx", "", "For forcing the ice srflx candidate IP address")
