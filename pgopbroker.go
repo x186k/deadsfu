761dac75a9e2ccfc46b2e1056aece6dd91a6a26d
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-12-30 19:43:24 -0800

    pgopbroker work
    chans now interface{}, XPacket
    remove subReplayCh:
      every subscriber gets a copy of the current GOP
      they can ignore it if they want

--- pgopbroker.go
+++ pgopbroker.go
@@ -1,36 +1,35 @@
 package main
 
-import "time"
-
 //credit for inspiration to https://stackoverflow.com/a/49877632/86375
 
 // the Partial GOP Broker
 // can publish/broadcast pkts
 // but can also serve up the current GOP so far
 type PgopBroker struct {
-	stopCh      chan struct{}
-	publishCh   chan XPacket
-	subCh       chan chan XPacket
-	subReplayCh chan chan XPacket
-	unsubCh     chan chan XPacket
+	stopCh    chan struct{}
+	publishCh chan xany
+	subCh     chan chan xany
+	unsubCh   chan chan xany
 }
+
+type xany interface{}
 
 var _ = NewPgopBroker
 
 func NewPgopBroker() *PgopBroker {
 	return &PgopBroker{
-		stopCh:      make(chan struct{}),
-		publishCh:   make(chan XPacket, 1),
-		subCh:       make(chan chan XPacket, 1),
-		subReplayCh: make(chan chan XPacket, 1),
-		unsubCh:     make(chan chan XPacket, 1),
+		stopCh:    make(chan struct{}),
+		publishCh: make(chan xany, 1),
+		subCh:     make(chan chan xany, 1),
+		unsubCh:   make(chan chan xany, 1),
 	}
 }
 
 func (b *PgopBroker) Start() {
-	buf := make([]XPacket, 0)
 
-	subs := map[chan XPacket]struct{}{}
+	var buf []XPacket = nil
+
+	subs := map[chan xany]struct{}{}
 	for {
 		select {
 		case <-b.stopCh:
@@ -38,31 +37,45 @@ func (b *PgopBroker) Start() {
 
 		case msgCh := <-b.subCh:
 			subs[msgCh] = struct{}{}
-
-		case msgCh := <-b.subReplayCh:
-			subs[msgCh] = struct{}{}
-			// start replay
-			go PgopReplay(buf, msgCh)
+			if buf != nil {
+				msgCh <- buf
+			} else {
+				msgCh <- []XPacket{}
+			}
 
 		case msgCh := <-b.unsubCh:
 			delete(subs, msgCh)
 
-		case m := <-b.publishCh:
+		case mm := <-b.publishCh:
 
-			if m.typ == Video { // save video GOPs
-				if m.keyframe || len(buf) > 50000 { // handle runaway growth
-					buf = make([]XPacket, 0) // XXX alloc optimize opportunity
+			switch m := mm.(type) {
+			case XPacket:
+				if m.typ == Video { // save video GOPs
+					if len(buf) > 50000 { //oversize protection
+						buf = nil
+					}
+					if m.keyframe {
+						buf = make([]XPacket, 0, 100)
+					}
+					if buf != nil {
+						buf = append(buf, m)
+					}
+					//XXXX remove me
+					// this sanity check moved to the receiving side
+					// if len(buf) > 0 && !buf[0].keyframe {
+					// 	panic("replay must begin with KF, or be empty")
+					// }
 				}
-				buf = append(buf, m)
-			}
-
-			for msgCh := range subs {
-				// msgCh is buffered, use non-blocking send to protect the broker:
-				select {
-				case msgCh <- m:
-				default:
-					pl("dropped packet/msg")
+				for msgCh := range subs {
+					// msgCh is buffered, use non-blocking send to protect the broker:
+					select {
+					case msgCh <- m:
+					default:
+						pl("dropped packet/msg")
+					}
 				}
+			default:
+				panic("xpacket only")
 			}
 		}
 	}
@@ -72,47 +85,16 @@ func (b *PgopBroker) Stop() {
 	close(b.stopCh)
 }
 
-func (b *PgopBroker) Subscribe(msgCh chan XPacket) {
+func (b *PgopBroker) Subscribe(msgCh chan xany) {
 	//msgCh := make(chan XPacket, 5)
 	b.subCh <- msgCh
 }
 
-func (b *PgopBroker) SubscribeReplay(msgCh chan XPacket) {
-	//msgCh := make(chan XPacket, 5)
-	b.subReplayCh <- msgCh
-}
-
-func (b *PgopBroker) UnsubscribeClose(msgCh chan XPacket) {
+func (b *PgopBroker) UnsubscribeClose(msgCh chan xany) {
 	close(msgCh)
 	b.unsubCh <- msgCh
 }
 
 func (b *PgopBroker) Publish(msg XPacket) {
 	b.publishCh <- msg
-}
-
-func PgopReplay(buf []XPacket, ch chan XPacket) {
-
-	pl("pgopreplay n:", len(buf))
-
-	if len(buf) == 0 {
-		return
-	}
-
-	delta := nanotime() - buf[0].now // linear regression or other would be better
-
-	for _, p := range buf {
-		deadline := p.now + delta
-		sleep := deadline - nanotime()
-
-		if sleep < 0 {
-			sleep = 0
-		}
-		time.Sleep(time.Duration(sleep))
-
-		//pl("pgop replay sending 1")
-		p.replay = true
-		ch <- p
-	}
-
 }
