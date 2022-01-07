9c5fc6372c4933664e97d242d9c9c87230418c10
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2022-01-07 14:35:52 -0800

    changed xbroker to single-chan, type switch operation

--- xbroker.go
+++ xbroker.go
@@ -15,14 +15,14 @@ The XBroker does these things:
 */
 
 type XBroker struct {
-	stopCh    chan struct{}
-	publishCh chan xany
-	subChCh   chan chan xany
-	unsubChCh chan chan xany
-
+	msgCh  chan xany
 	txtsMu sync.Mutex
 	txts   map[*TxTrack]struct{}
 }
+
+type XBrokerMsgStop struct{} // https://goplay.tools/snippet/9K4u1ESBg6A
+type XBrokerMsgSub chan xany
+type XBrokerMsgUnSub chan xany
 
 type xany interface{} // go 1.18 is here soon
 
@@ -30,11 +30,8 @@ var _ = NewXBroker
 
 func NewXBroker() *XBroker {
 	return &XBroker{
-		stopCh:    make(chan struct{}),
-		publishCh: make(chan xany, 1),
-		subChCh:   make(chan chan xany, 1),
-		unsubChCh: make(chan chan xany, 1),
-		txts:      make(map[*TxTrack]struct{}),
+		msgCh: make(chan xany, 1),
+		txts:  make(map[*TxTrack]struct{}),
 	}
 }
 
@@ -44,104 +41,95 @@ func (b *XBroker) Start() {
 
 	subs := make(map[chan xany]struct{})
 
-	for {
-		select {
-		case <-b.stopCh:
-			return
+	for mm := range b.msgCh {
 
-		case msgCh := <-b.subChCh:
-			subs[msgCh] = struct{}{}
+		switch m := mm.(type) {
+		case XBrokerMsgSub:
+			subs[m] = struct{}{}
 			if buf != nil {
-				msgCh <- buf
+				m <- buf
 			} else {
-				msgCh <- []XPacket{}
+				m <- []XPacket{}
+			}
+		case XBrokerMsgUnSub:
+			delete(subs, m)
+		case XBrokerMsgStop:
+			return
+		case XPacket:
+			// STEP1: we save video XPacket's in the gop-so-far
+			if m.typ == Video { // save video GOPs
+				if len(buf) > 50000 { //oversize protection // XXX >cap(buf)
+					buf = nil
+				}
+				if m.keyframe {
+					buf = make([]XPacket, 0, 100) // XXX pool?
+
+				}
+				if buf != nil {
+					buf = append(buf, m)
+				}
+
+				// this sanity check moved to the receiving side
+				// if len(buf) > 0 && !buf[0].keyframe {
+				// 	panic("replay must begin with KF, or be empty")
+				// }
+			}
+			//STEP2 we send it to all chan-subscribers
+			for msgCh := range subs {
+				// msgCh is buffered, use non-blocking send to protect the broker:
+				// select {
+				// case msgCh <- m:
+				// default:
+				// 	pl("dropped packet/msg")
+				// }
+				msgCh <- m
+			}
+			//STEP3 send the packet to tracks
+
+			if m.typ != Video { // save video GOPs
+				break
 			}
 
-		case msgCh := <-b.unsubChCh:
-			delete(subs, msgCh)
+			b.txtsMu.Lock()
+			//pl(len(b.txts))
 
-		case mm := <-b.publishCh:
+			for txt := range b.txts {
+				copy := m.pkt
 
-			switch m := mm.(type) {
-			case XPacket:
-				// STEP1: we save video XPacket's in the gop-so-far
-				if m.typ == Video { // save video GOPs
-					if len(buf) > 50000 { //oversize protection // XXX >cap(buf)
-						buf = nil
-					}
-					if m.keyframe {
-						buf = make([]XPacket, 0, 100) // XXX pool?
+				// if you accidentially pass a copy of the first param, seqno gunna always be one/wrong on other side
+				SpliceRTP(&txt.splicer, &copy, m.now, int64(txt.clockrate)) // writes all over m.pkt.Header
+				//pl(888,m.pkt.SequenceNumber,m.pkt.Timestamp,len(b.txts))
 
-					}
-					if buf != nil {
-						buf = append(buf, m)
-					}
-
-					// this sanity check moved to the receiving side
-					// if len(buf) > 0 && !buf[0].keyframe {
-					// 	panic("replay must begin with KF, or be empty")
-					// }
+				err := txt.track.WriteRTP(&copy) // faster than packet.Write()
+				if err == io.ErrClosedPipe {
+					panic("unexpected ErrClosedPipe")
+				} else if err != nil {
+					errlog.Println(err.Error())
 				}
-				//STEP2 we send it to all chan-subscribers
-				for msgCh := range subs {
-					// msgCh is buffered, use non-blocking send to protect the broker:
-					// select {
-					// case msgCh <- m:
-					// default:
-					// 	pl("dropped packet/msg")
-					// }
-					msgCh <- m
-				}
-				//STEP3 send the packet to tracks
-
-				if m.typ != Video { // save video GOPs
-					break
-				}
-
-				b.txtsMu.Lock()
-				//pl(len(b.txts))
-
-				for txt := range b.txts {
-					copy := m.pkt
-
-					// if you accidentially pass a copy of the first param, seqno gunna always be one/wrong on other side
-					SpliceRTP(&txt.splicer, &copy, m.now, int64(txt.clockrate)) // writes all over m.pkt.Header
-					//pl(888,m.pkt.SequenceNumber,m.pkt.Timestamp,len(b.txts))
-
-					err := txt.track.WriteRTP(&copy) // faster than packet.Write()
-					if err == io.ErrClosedPipe {
-						panic("unexpected ErrClosedPipe")
-					} else if err != nil {
-						errlog.Println(err.Error())
-					}
-				}
-
-				b.txtsMu.Unlock()
-
-			default:
-				panic("xpacket only")
 			}
+
+			b.txtsMu.Unlock()
 
 		}
 	}
 }
 
 func (b *XBroker) Stop() {
-	close(b.stopCh)
+	close(b.msgCh)
 }
 
 func (b *XBroker) Subscribe(msgCh chan xany) {
 	//msgCh := make(chan XPacket, 5)
-	b.subChCh <- msgCh
+	b.msgCh <- XBrokerMsgSub(msgCh)
 }
 
 func (b *XBroker) UnsubscribeClose(msgCh chan xany) {
 	close(msgCh)
-	b.unsubChCh <- msgCh
+	b.msgCh <- XBrokerMsgUnSub(msgCh)
 }
 
 func (b *XBroker) Publish(msg XPacket) {
-	b.publishCh <- msg
+	b.msgCh <- msg
 }
 
 func (b *XBroker) AddTrack(txt *TxTrack) {
