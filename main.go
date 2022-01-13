fecf4fe08a23aba5ec7e0d21b9686383218ba061
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2022-01-13 12:52:59 -0800

    exploratory work on a mutex based approach to room switching

--- main.go
+++ main.go
@@ -115,6 +115,21 @@ type roomState struct {
 	roomname    string
 	ingressSema *semaphore.Weighted // is a publisher already using '/foobar' ??
 	xBroker     *XBroker
+	live        *SubList // writes allowed when this lock taken
+	pending     *SubList // writes not allowed when this taken
+}
+
+type SubList struct {
+	mu   sync.Mutex
+	subs map[*Subscriber]struct{}
+}
+
+type Subscriber struct {
+	mu      sync.Mutex
+	video   TxTrack
+	audio   TxTrack
+	live    *SubList
+	pending *SubList
 }
 
 var roomMap = make(map[string]*roomState)
@@ -2308,4 +2323,16 @@ X:
 
 	close(done)
 
+}
+
+// set of outbound/tx tracks associated with subscriber
+type TxSet struct {
+	aud TxTrack
+	vid TxTrack
+}
+
+// a group of aud/vid tracks for more than one subscriber
+type TxGroup struct {
+	mu     sync.Mutex          // held during map changes AND during WriteRTP
+	tracks map[*TxSet]struct{} //
 }
