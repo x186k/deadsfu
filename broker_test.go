fee4d3531bea789acfe7b2c97d3b768106c35d6c
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2022-02-20 15:36:17 -0800

    add ns/write using b.ReportMetric

--- broker_test.go
+++ broker_test.go
@@ -5,17 +5,42 @@ import (
 	//"sync"
 	"log"
 	"os"
+	"reflect"
+	"sync"
 	"testing"
 	"time"
+	"unsafe"
 
 	"github.com/pion/rtp"
 )
+
+// c@macmini ~/D/deadsfu (main) [1]> go test -bench='^BenchmarkBrokerWithWriter' . -run='^$' .
+// goos: darwin
+// goarch: amd64
+// pkg: github.com/x186k/deadsfu
+// cpu: Intel(R) Core(TM) i5-8500B CPU @ 3.00GHz
+// BenchmarkBrokerWithWriter1PairsPool-6        	 4031882	       302.8 ns/op	       302.8 ns/write	      33 B/op	       0 allocs/op
+// BenchmarkBrokerWithWriter10PairsPool-6       	 3100899	       422.4 ns/op	        42.24 ns/write	      33 B/op	       0 allocs/op
+// BenchmarkBrokerWithWriter100PairsPool-6      	  693356	      1987 ns/op	        19.87 ns/write	      32 B/op	       0 allocs/op
+// BenchmarkBrokerWithWriter1000PairsPool-6     	   87928	     20177 ns/op	        20.18 ns/write	      15 B/op	       0 allocs/op
+// BenchmarkBrokerWithWriter1PairsNoPool-6      	 3432055	       359.8 ns/op	       359.8 ns/write	     181 B/op	       2 allocs/op
+// BenchmarkBrokerWithWriter10PairsNoPool-6     	 2623671	       438.1 ns/op	        43.81 ns/write	     181 B/op	       2 allocs/op
+// BenchmarkBrokerWithWriter100PairsNoPool-6    	  609979	      1992 ns/op	        19.92 ns/write	     181 B/op	       2 allocs/op
+// BenchmarkBrokerWithWriter1000PairsNoPool-6   	   56983	     20324 ns/op	        20.32 ns/write	     182 B/op	       2 allocs/op
+// PASS
+// ok  	github.com/x186k/deadsfu	12.716s
 
 var N int
 
 func TestMain(m *testing.M) {
 	log.SetFlags(log.Lshortfile)
 	os.Exit(m.Run())
+}
+
+var pool = sync.Pool{
+	// New optionally specifies a function to generate
+	// a value when Get would otherwise return nil.
+	New: func() interface{} { return new(XPacket) },
 }
 
 func BenchmarkBrokerNoWrite(b *testing.B) {
@@ -25,12 +50,6 @@ func BenchmarkBrokerNoWrite(b *testing.B) {
 
 	c := a.Subscribe()
 	<-c
-
-	// pp := sync.Pool{
-	// 	// New optionally specifies a function to generate
-	// 	// a value when Get would otherwise return nil.
-	// 	New: func() interface{} { return new(XPacket) },
-	// }
 
 	d := make(chan struct{})
 	go func() {
@@ -75,17 +94,32 @@ func (z *Z) WriteRTP(p *rtp.Packet) error {
 	return nil
 }
 
-func BenchmarkBrokerWithWriter1Pairs(b *testing.B) {
-	benchmarkBrokerWithWriter(b, 1)
+func BenchmarkBrokerWithWriter1PairsPool(b *testing.B) {
+	benchmarkBrokerWithWriter(b, 1, true)
 }
-func BenchmarkBrokerWithWriter10Pairs(b *testing.B) {
-	benchmarkBrokerWithWriter(b, 10)
+func BenchmarkBrokerWithWriter10PairsPool(b *testing.B) {
+	benchmarkBrokerWithWriter(b, 10, true)
 }
-func BenchmarkBrokerWithWriter100Pairs(b *testing.B) {
-	benchmarkBrokerWithWriter(b, 100)
+func BenchmarkBrokerWithWriter100PairsPool(b *testing.B) {
+	benchmarkBrokerWithWriter(b, 100, true)
+}
+func BenchmarkBrokerWithWriter1000PairsPool(b *testing.B) {
+	benchmarkBrokerWithWriter(b, 1000, true)
+}
+func BenchmarkBrokerWithWriter1PairsNoPool(b *testing.B) {
+	benchmarkBrokerWithWriter(b, 1, false)
+}
+func BenchmarkBrokerWithWriter10PairsNoPool(b *testing.B) {
+	benchmarkBrokerWithWriter(b, 10, false)
+}
+func BenchmarkBrokerWithWriter100PairsNoPool(b *testing.B) {
+	benchmarkBrokerWithWriter(b, 100, false)
+}
+func BenchmarkBrokerWithWriter1000PairsNoPool(b *testing.B) {
+	benchmarkBrokerWithWriter(b, 1000, false)
 }
 
-func benchmarkBrokerWithWriter(b *testing.B, numpairs int) {
+func benchmarkBrokerWithWriter(b *testing.B, numwrites int, poollike bool) {
 
 	a := NewXBroker()
 	go a.Start()
@@ -113,7 +147,7 @@ func benchmarkBrokerWithWriter(b *testing.B, numpairs int) {
 
 	trks := NewTxTracks()
 
-	for i := 0; i < numpairs; i++ {
+	for i := 0; i < numwrites; i++ {
 		vidwriter := &Z{}
 		pair := TxTrackPair{
 			aud: TxTrack{
@@ -137,14 +171,27 @@ func benchmarkBrokerWithWriter(b *testing.B, numpairs int) {
 	b.ReportAllocs()
 	b.ResetTimer()
 
-	for i := 0; i < b.N; i++ {
-		//p := pp.Get().(*XPacket)
+	var p *XPacket
 
-		p := XPacket{
+	if poollike {
+		p = &XPacket{
 			arrival:  0,
 			pkt:      &rtp.Packet{},
 			typ:      0,
 			keyframe: false,
+		}
+	}
+
+	for i := 0; i < b.N; i++ {
+		//p := pool.Get().(*XPacket)
+		//p.pkt = &rtp.Packet{}
+		if !poollike {
+			p = &XPacket{
+				arrival:  0,
+				pkt:      &rtp.Packet{},
+				typ:      0,
+				keyframe: false,
+			}
 		}
 
 		p.typ = Video
@@ -153,15 +200,20 @@ func benchmarkBrokerWithWriter(b *testing.B, numpairs int) {
 		} else {
 			p.keyframe = false
 		}
-		a.Publish(&p)
+		a.Publish(p)
 
 	}
 	b.StopTimer()
 	a.Unsubscribe(c)
 
 	<-d
+	time.Sleep(time.Millisecond * 5) // wait for drain
 
-	time.Sleep(time.Second)
+	durintf :=
+		GetUnexportedField(reflect.ValueOf(b).Elem().FieldByName("duration"))
+	duration := durintf.(time.Duration)
+
+	b.ReportMetric(float64(duration)/float64(b.N)/float64(numwrites), "ns/write")
 
 	//data race
 	// nn:=0
@@ -172,6 +224,8 @@ func benchmarkBrokerWithWriter(b *testing.B, numpairs int) {
 	// log.Println("num writes", nn)
 }
 
+func GetUnexportedField(field reflect.Value) interface{} {
+	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
 }
 
 // func TestNanosecondsPerSubscriberSend(b *testing.T) {
